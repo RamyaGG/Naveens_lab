18d6571624510059b209c6bbd07389f13982b171
Ramya GG <37952871+RamyaGG@users.noreply.github.com>
2018-12-25 15:25:52 +0530

    Modified for loop

--- src/javaInterviewQuestions/StarPyramid.java
+++ src/javaInterviewQuestions/StarPyramid.java
@@ -1,6 +1,6 @@
 package javaInterviewQuestions;
 
-/*
+															/*
      * 
     * * 
    * * * 
@@ -8,22 +8,22 @@ package javaInterviewQuestions;
  * * * * * 
 * * * * * * 
 
- */
+ 															*/
 
 public class StarPyramid {
 
 	public static void main(String[] args) {
 		int n=6;
-		for (int i=0; i<n; i++) 
+		for (int i=1; i<=n; i++) 
         {  
-            for (int j=n-i; j>1; j--) 
+            for (int j=1; j<=n-i; j++) 
             { 
                 System.out.print(" "); 
             } 
    
             //  inner loop to handle number of columns 
             //  values changing acc. to outer loop 
-            for (int j=0; j<=i; j++ ) 
+            for (int j=1; j<=i; j++ ) 
             { 
                 System.out.print("* "); 
             } 
